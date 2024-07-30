8c49e8733d9e207a92cb90230628328ffe85ffc2
simrangupta234 <simrangupta172002@gmail.com>
2024-07-30 21:49:06 +0530

    updated

--- server.js
+++ server.js
@@ -22,12 +22,13 @@ const upload = multer({ storage: storage });
 connectDb();
 app.use(
   cors({
-    origin: "http://127.0.0.1:5173",
+    origin: ["http://127.0.0.1:5173", "http://localhost:5173"],
     credentials: true,
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     optionsSuccessStatus: 204,
   })
 );
+
 
 app.post("/api/movies", upload.array("testImage", 6), async (req, res) => {
   const { id, title, release_year, duration, genre, overview, starring } =
